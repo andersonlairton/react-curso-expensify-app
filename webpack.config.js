e4fbd79c9c06e02800feedb57752c5a967ee4ec2
Anderson Lairton <lairton.anderson@gmail.com>
2019-08-06 18:45:13 -0300

    setup devDependencies and dis folder Aula 137

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports=(env)=>
     output:
     {
         
-        path:path.join(__dirname,'public'),
+        path:path.join(__dirname,'public','dist'),
         filename:'bundle.js'
     },
     module:
@@ -53,6 +53,7 @@ module.exports=(env)=>
     devServer:
     {
         contentBase:path.join(__dirname,'public'),
-        historyApiFallback:true //pelo que entendi,esta pegando a pgina anterior e apresentando em uma nova com novos dados
+        historyApiFallback:true ,//pelo que entendi,esta pegando a pgina anterior e apresentando em uma nova com novos dados
+        publicPath:'/dist/'
     }}
 }
